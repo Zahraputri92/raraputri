09914e1c377a5e28f3126a4b5967449b5eaba25f
rara cahyani <zcahyani92@gmail>
2024-11-18 19:26:02 +0700

    Upload Project React

---
+++ src/services/getAllProductCategories.js
@@ -0,0 +1,19 @@
+export default function getAllProductCategories(){
+  return  [   {
+      id: '1',
+      slug: 'men-bracelet',
+      name: `Men's Bracelet`
+    },
+    {
+      id: '2',
+      slug: 'women-bracelet',
+      name: `Women's Bracelet`
+    },
+    {
+      id: '3',
+      slug: `couple-bracelet`,
+      name: 'Couple Bracelet',
+    },
+   
+  ]
+}
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,103 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: '1',
+      slug: 'onyx-warrior-band',
+      name: "Onyx Warrior Band",
+      category: `Men's Bracelet`,
+      categorySlug: 'men-bracelet',
+      price: 200_000,
+      stock: 100,
+      imageUrl: '/assets/images/black.jpg',
+      description: `Men's bracelet with matte black onyx beads, a symbol of strength and determination. It is perfect for modern men who love simple yet meaningful accessories.`
+    },
+    {
+      id: '2',
+      slug: 'vintage-leather-wrap',
+      name: "Vintage Leather Wrap",
+      category: `Men's Bracelet`,
+      categorySlug: 'men-bracelet',
+      price: 175_000,
+      stock: 100,
+      imageUrl: '/assets/images/leather.jpg',
+      description: `Genuine leather bracelet with silver metallic accents, suitable for casual or semi-formal style. It gives a masculine and tough impression.`
+    },
+    {
+      id: '3',
+      slug: 'classic-lava-stone',
+      name: "Classic Lava Stone",
+      category: `Men's Bracelet`,
+      categorySlug: 'men-bracelet',
+      price: 160_000,
+      stock: 100,
+      imageUrl: `/assets/images/lava.jpg`,
+      description: `The bracelet is with natural black lava stones that give it a strong and masculine feel. Equipped with geometric engraved metal accents for a modern touch. Suitable for daily activities and casual events.`
+    },
+    {
+      id: '4',
+      slug: 'radiant-pearl-charm',
+      name: "Radiant Pearl Charm",
+      category: `Women's Bracelet`,
+      categorySlug: 'women-bracelet',
+      price: 150_000,
+      stock: 100,
+      imageUrl: `/assets/images/radiant.jpg`,
+      description: `A bracelet that combines synthetic pearl beads with a heart-shaped charm. Provides an elegant touch for both formal and casual events.`
+    },
+    {
+      id: '5',
+      slug: 'vintage-boho-beaded-bracelet',
+      name: "Vintage Boho Beaded Bracelet",
+      category: `Women's Bracelet`,
+      categorySlug: 'women-bracelet',
+      price: 130_000,
+      stock: 100,
+      imageUrl: `/assets/images/tassel.jpg`,
+      description: `Bohemian bracelets are stacked with a combination of colorful wooden beads and natural stones. The unique ethnic design is suitable for both casual and leisure styles.`
+    },
+    {
+      id: '6',
+      slug: 'rose-quartz-charm-bracelet ',
+      name: "Rose Quartz Charm Bracelet",
+      category: `Women's Bracelet`,
+      categorySlug: 'women-bracelet',
+      price: 200_000,
+      stock: -10,
+      imageUrl: `/assets/images/quartz.jpg`,
+      description: `The beaded bracelet is made of rose quartz stones in soft pink, believed to bring the energy of love and tranquility. Decorated with a heart-shaped pendant for a romantic look.`
+    },
+    {
+      id: '7',
+      slug: 'white-elegance-pearl-bracelet ',
+      name: "White Elegance Pearl Bracelet",
+      category: `Women's Bracelet`,
+      categorySlug: 'women-bracelet',
+      price: 150_000,
+      stock: 100,
+      imageUrl: `/assets/images/elegance.jpg`,
+      description: `Beautiful bracelet with elegantly assembled white synthetic pearls. Perfect for formal occasions or to complement everyday feminine styles. Equipped with stainless steel stainless steel hooks.`
+    },
+    {
+      id: '8',
+      slug: 'magnetic-stone-couple-bracelet',
+      name: "Magnetic Stone Couple Bracelet",
+      category: `Couple Bracelet`,
+      categorySlug: 'couple-bracelet',
+      price: 170_000,
+      stock: 5,
+      imageUrl: `/assets/images/stone.jpg`,
+      description: `An exclusive pair bracelet with a combination of black hematite natural stones and natural white beads, complemented by a magnetic feature in the center. This magnet symbolizes a mutually attractive and close relationship. This bracelet is suitable for couples who want to always feel close even when they are far away.`
+    },
+    {
+      id: '9',
+      slug: 'couple-heart-decor-bracelet',
+      name: "Couple Heart Decor Bracelet",
+      category: `Couple Bracelet`,
+      categorySlug: 'couple-bracelet',
+      price: 130_000,
+      stock: 20,
+      imageUrl: `/assets/images/heart.jpg`,
+      description: `Couple bracelets with heart-shaped pendants that complement each other when brought together. Specially designed to symbolize united love. The bracelet strap is made of high-quality nylon material that is comfortable to wear all day long. Stainless steel pendant accents add an elegant and durable impression. Perfect for romantic gifts on anniversaries, birthdays, or Valentine's.`
+    },
+  ]
+}
